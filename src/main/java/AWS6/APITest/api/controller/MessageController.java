3cdc64ad15e36bbdcf5d7781567d22421e215a8f
gytjd <hys339631@gmail.com>
2024-08-06 23:17:43 +0900

    TEST

--- src/main/java/AWS6/APITest/api/controller/MessageController.java
+++ src/main/java/AWS6/APITest/api/controller/MessageController.java
@@ -1,21 +1,32 @@
 package AWS6.APITest.api.controller;
 
-import AWS6.APITest.service.message.MessageProducer;
+
+import AWS6.APITest.api.dto.message.MessageDto;
+import AWS6.APITest.service.MessageService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/api/messages")
+@Slf4j
 @RequiredArgsConstructor
+@RestController
 public class MessageController {
 
-    private final MessageProducer messageProducer;
+    private final MessageService messageService;
 
-    @PostMapping
-    public void sendMessage(@RequestBody String message) {
-        messageProducer.sendMessage(message);
+    /**
+     * Queue로 메시지를 발행
+     *
+     * @param messageDto 발행할 메시지의 DTO 객체
+     * @return ResponseEntity 객체로 응답을 반환
+     */
+    @RequestMapping(value = "/send/message", method = RequestMethod.POST)
+    public ResponseEntity<?> sendMessage(@RequestBody MessageDto messageDto) {
+        messageService.sendMessage(messageDto);
+        return ResponseEntity.ok("Message sent to RabbitMQ!");
     }
 }
